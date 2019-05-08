c81cd71bdccda7b333dd13347ec697baa45f85d1
Sally Beckwith <sallypbeckwith@gmail.com>
2019-05-08 16:37:20 -0400

    Cards shuffle when click occurs

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import PlayerCard from "../PlayerCard"
 import "./style.css";
 
 // GameBoard renders a region for displaying cards for game
-class GameBoard extends React.Component {
+class GameBoard extends Component {
 
-  // const {
-  //   players
-  // } = this.props;
-
-  state = {
-    numClicked: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      numClicked: 0,
+      players: this.props.players
+    }
   }
 
-  handleClickCount = () => {
-    this.setState({ numClicked: this.state.numClicked + 1 })
+  // return a random index number from an array
+  getRandIndex = (arr) => {
+    return Math.floor(Math.random() * arr.length)
   }
 
+  shuffleCards = () => {
+    let array = this.state.players;
+    const arrLength = array.length;
+    let shuffled = [];
+    for (let i = 0; i < arrLength; i++ ){
+      let index = this.getRandIndex(array);
+      shuffled.push(array[index]);
+      array.splice(index,1);
+    }
+    return shuffled;
+  }
+  
+    handleClickCount = () => {
+      const newPlayersArray = this.shuffleCards();
+      // console.log('newPlayersArray');
+      // console.log(newPlayersArray);
+      this.setState({
+        numClicked: this.state.numClicked + 1,
+        players: newPlayersArray
+      })
+    }
+  
   render() {
     return (
       <div className="row">
         <h1>Clicked: {this.state.numClicked} </h1>
         <div className="col-md-10  mx-auto game-board">
-          { this.props.players.map( (p) => { return( 
+          { this.state.players.map( (p) => { return( 
             <PlayerCard key={p.name} name={p.name} image={p.image} updateCount={this.handleClickCount}/>
           ) }) }
 
--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -3,20 +3,27 @@ import "./style.css";
 
 // PlayerCard renders an image
 class PlayerCard extends React.Component {
-  state = {
-    clicked: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+
+  }
 
   handleClick = () => {
-    console.log(`${this.props.name} was clicked.`);
+    if (this.state.clicked) {
+      alert('you lost');
+    }
     this.props.updateCount();
     this.setState({ clicked: true })
   }
 
   render() {
     return (
-      <div className="card w-25 wrapper" onClick={this.handleClick}>
+      <div className="card w-25 " onClick={this.handleClick}>
         <img className="card-img" alt={this.props.name} src={this.props.image} />
+        <div><h2>{this.state.clicked ? "true" : "false"}</h2></div>
       </div>
     )
   }

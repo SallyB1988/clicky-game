12f0b849c616966b3ed46edbf326f341bf2b2295
Sally Beckwith <sallypbeckwith@gmail.com>
2019-05-08 22:10:25 -0400

    Updates

---
+++ precache-manifest.2b21b0d5fc90197f4da63ad337096908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0900e652bda8af7d272e595e5c925ea4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "49380b059de62a32e0e3",
+    "url": "/clicky-game/static/css/main.c0b8cfbe.chunk.css"
+  },
+  {
+    "revision": "51405c61520c1c0e0eec",
+    "url": "/clicky-game/static/js/2.c8b1c68a.chunk.js"
+  },
+  {
+    "revision": "49380b059de62a32e0e3",
+    "url": "/clicky-game/static/js/main.58ee4fd8.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);

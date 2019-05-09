dd5203a56c1878b871b5b161a5f323bc60537727
Sally Beckwith <sallypbeckwith@gmail.com>
2019-05-09 08:06:25 -0400

    trying to get images to display in deployed version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import GameBoard from './components/GameBoard';
-import players from './players.json';
+import players from './data/players.json';
 
 function App() {
   return( 
